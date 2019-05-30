11aa9ac7c12f6a8de13c5edcc5adeb7c062d9fdc
Anthony Riojas <anthonyr@Anthonys-MacBook-Pro.local>
2019-05-30 11:09:31 -0700

    js control flow challenge

---
+++ js/bouncer.js
@@ -0,0 +1,26 @@
+var attendees = [
+    {name: 'Xev', age: 35},
+    {name: 'Mal', age: 29},
+    {name: 'Clos', age: 17},
+    {name: 'Gysh', age: 39},
+    {name: 'Zek', age: 10},
+    {name: 'Hops', age: 14},
+    {name: 'Sil', age: 21}
+  ];
+
+  var adults = [];
+  var minors = [];
+
+  for (var person of attendees) {
+      if (person.age >= 18) {
+          adults.push(person);
+     
+      } else {
+          minors.push(person);
+      } 
+  }
+
+  console.log(adults);
+  console.log(minors);
+// in class solution
+
---
+++ js/shipping.js
@@ -0,0 +1,13 @@
+var truck = [];
+var truckCapacity = 8;
+var packages = ['skis', 'DVD', 'frisbee', 'computer', 'pizza', 'fishing pole', 'car battery', 'stereo', 'furniture', 'salad dressing', 'sunshine', 'daydreams'];
+
+// in class solution
+var loadingDock;
+
+while (truck.length <= truckCapacity) {
+    loadingDock = packages.pop();
+    truck.push( package.pop() );
+}
+
+console.log()

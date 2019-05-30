a482b7fc122b7488111735a5f67bdf59d3e80b97
Anthony Zardis <anthonyzardis@gmail.com>
2019-05-30 10:16:51 -0700

    Added loop to fizzbuzz

--- js/fizzbuzz.js
+++ js/fizzbuzz.js
@@ -5,14 +5,15 @@
 //number. This is a very common interview question!
 
 
-var num = 20 - 2;
-
-if (num % 3 === 0 && num % 5 === 0) {
-    console.log( 'fizzbuzz' );
-}else if (num % 3 === 0) {
-    console.log( 'fizz' );
-} else if (num % 5 === 0) {
-    console.log( 'buzz' );
-} else {
-    console.log( num );
+//var num = 20 - 2;
+for (var num = 0; num <= 100; num++) {
+    if (num % 3 === 0 && num % 5 === 0) {  // could refactor to !(num % 3) and !(num % 5)
+        console.log( 'fizzbuzz' );
+    }else if (num % 3 === 0) {
+        console.log( 'fizz' );
+    } else if (num % 5 === 0) {
+        console.log( 'buzz' );
+    } else {
+        console.log( num );
+    }
 }
